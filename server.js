3edc41f9bc791fcf37f3376b7e258773a41bea86
Julian Seyfarth <julian.seyfarth@web.de>
2022-09-12 14:43:19 -0500

    change to light version without angular

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname + '/dist/jjs-travel-blog'))
+app.use(express.static(__dirname));
 
 app.get('/*', (req, resp) => {
-    resp.sendFile(__dirname + '/dist/jjs-travel-blog/index.html');
+    resp.sendFile(__dirname + '/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
